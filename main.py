16389182c5e87eacd3c8e04553b48ec8c60dc3e7
EagerlyBecomingHuman <kjkaye7@gmail.com>
2024-09-30 16:49:53 -0400

    drew player

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,18 +9,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    # x = Player(SCREEN_WIDTH) / 2
+    # y = Player(SCREEN_HEIGHT) / 2
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "black")
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # limits the framerate to 60 FPS

4c083f49712f8ef9c70daa41f555dc1a9e8a36a3
EagerlyBecomingHuman <kjkaye7@gmail.com>
2024-09-30 09:40:54 -0400

    added FPS limit

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000  # limits the framerate to 60 FPS
 
 
 if __name__ == "__main__":

d728ac82b073250b4a9d891933da851a0da31962
EagerlyBecomingHuman <kjkaye7@gmail.com>
2024-10-07 16:08:41 -0400

    destoying asteroids

--- main.py
+++ main.py
@@ -49,6 +49,11 @@ def main():
             if asteroid.collision_check(player):
                 print("Game over!")
                 sys.exit
+            
+            for shot in shots:
+                if asteroid.collision_check(shot):
+                    shot.kill()
+                    asteroid.kill()
 
         screen.fill("black")
 
